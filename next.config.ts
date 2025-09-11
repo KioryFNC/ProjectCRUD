0454da3616d47f847d1d9ffc62d584e76ddd2583
KioryFNC <yuri.barros.luz.familia@gmail.com>
2025-09-11 01:06:18 -0300

    fix img next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api-teste-front-production.up.railway.app",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,12 +1,91 @@
 "use client";
 
+import { Product } from "@/interfaces/Product";
+import { useEffect, useState } from "react";
+import api from "@/app/lib/api";
+import Image from "next/image";
+
 export default function ProductsPage() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await api.get("/products");
+        setProducts(response.data.data);
+      } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+        alert("Não foi possivel carregar os produtos");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <span className="loading loading-lg loading-spinner text-primary"></span>
+      </div>
+    );
+  }
+
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold text-base-content">Meus Produtos</h1>
-      <p className="mt-2 text-base-content/70">
-        Em Breve, listarei os produtos da APi aqui
-      </p>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Meus Produtos</h1>
+        <button className="btn btn-bg-primary">Adicionar Produto</button>
+      </div>
+
+      <div className="mt-8 overflow-x-auto">
+        <table className="table-zebra table">
+          <thead>
+            <tr>
+              <th>Thumbnail</th>
+              <th>Titulo</th>
+              <th>Status</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>
+                  <div className="avatar">
+                    <div className="w-16 rounded">
+                      <Image
+                        src={product.thumbnail}
+                        alt={product.title}
+                        width={64}
+                        height={64}
+                        className="rounded"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td className="font-bold">{product.title}</td>
+                <td>
+                  {product.status ? (
+                    <div className="badge badge-success">Ativo</div>
+                  ) : (
+                    <div className="badge badge-error">Inativo</div>
+                  )}
+                </td>
+                <td className="space-x-2">
+                  <button className="btn btn-sm btn-info">Editar</button>
+                  <button className="btn btn-sm btn-error">Deletar</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {products.length === 0 && (
+          <p className="py-8 text-center">Nenhum cadastrado ainda</p>
+        )}
+      </div>
     </div>
   );
 }
